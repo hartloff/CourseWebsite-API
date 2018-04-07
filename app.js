4b3b57bc0aecbcc8b2be858e73e511d578d787c2
jessehartloff <hartloff@buffalo.edu>
2018-04-06 21:21:15 -0400

    allow cross site requests

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ var api_router = require('./routes/api_router');
 
 var app = express();
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
